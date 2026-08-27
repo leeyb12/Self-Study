76522deb553e8999c191807792db60539d3f03fd
Yunbeom <dbsqja1233@naver.com>
2026-08-27 10:41:07 +0900

    lyb

--- 008/실전! 스프링 부트 3 & 리액트로 시작하는 모던 웹 애플리케이션 개발/Chapter 03/src/main/java/com/packt/cardatabase/domain/Car.java
+++ 008/실전! 스프링 부트 3 & 리액트로 시작하는 모던 웹 애플리케이션 개발/Chapter 03/src/main/java/com/packt/cardatabase/domain/Car.java
@@ -1,9 +1,12 @@
 package com.packt.cardatabase.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
 
 @Entity
 public class Car {
@@ -20,5 +23,81 @@ public class Car {
 
     public Car(String brand, String model, String color, String registrationNumber, int modelYear, int price, Owner owner) {
         super();
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.registrationNumber = registrationNumber;
+        this.modelYear = modelYear;
+        this.price = price;
+        this.owner = owner;
+    }
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "owner")
+    private Owner owner;
+
+    // Getter and setter
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public int getModelYear() {
+        return modelYear;
+    }
+
+    public void setModelYear(int modelYear) {
+        this.modelYear = modelYear;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
     }
 }
